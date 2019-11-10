86321421d3fea231a4d0919461bfb810bcb5801b
pawelmysior <pawelmysior@gmail.com>
2019-11-10 14:40:05 +0100

    Część 2 - drugi test, automatyczne migrowanie bazy danych i użycie fabryki modelu Post

--- tests/Feature/PostsTest.php
+++ tests/Feature/PostsTest.php
@@ -3,16 +3,19 @@
 namespace Tests\Feature;
 
 use App\Post;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class PostsTest extends TestCase
 {
+    use RefreshDatabase;
+
     /** @test */
     public function the_posts_show_route_can_be_accessed()
     {
         // Arrange
         // Dodajemy do bazy danych wpis
-        $post = Post::create([
+        $post = factory(Post::class)->create([
             'title' => 'Wrabiał krowę w morderstwo cioci',
         ]);
 
@@ -24,5 +27,17 @@ class PostsTest extends TestCase
         // Sprawdzamy że w odpowiedzi znajduje się tytuł wpisu
         $response->assertStatus(200)
             ->assertSeeText('Wrabiał krowę w morderstwo cioci');
+    }
+
+    /** @test */
+    public function the_body_attribute_is_shown_on_the_posts_show_view()
+    {
+        $post = factory(Post::class)->create([
+            'body' => 'Mroczna tajemnica mordu w oborze długo spędzała sen z oczu policjantom z Lublina. Kto zabił 88-letnią kobietę i jej krowę?',
+        ]);
+
+        $response = $this->get('/posts/' . $post->id);
+
+        $response->assertSeeText('Mroczna tajemnica mordu w oborze długo spędzała sen z oczu policjantom z Lublina. Kto zabił 88-letnią kobietę i jej krowę?');
     }
 }
